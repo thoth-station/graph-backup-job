9629852d06a29cbad9c8a4077cdbfbd2aeb20215
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2019-12-19 14:21:00 +0100

    Use new dump logic that rotates files stored

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Graph Backup Job
-# Copyright(C) 2019 Harshad Reddy Nalla.
+# Copyright(C) 2019 Harshad Reddy Nalla, Fridolin Pokorny
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,10 +17,8 @@
 
 """Graph backup logic for the Thoth project."""
 
-import sys
 import logging
 import os
-from datetime import datetime
 from thoth.analyzer import run_command
 from thoth.common import init_logging
 from thoth.storages import __version__ as __storages__version__
@@ -33,40 +31,27 @@ KNOWLEDGE_GRAPH_HOST = os.getenv("KNOWLEDGE_GRAPH_HOST", "localhost")
 KNOWLEDGE_GRAPH_PORT = os.getenv("KNOWLEDGE_GRAPH_PORT", "5432")
 KNOWLEDGE_GRAPH_USER = os.getenv("KNOWLEDGE_GRAPH_USER", "postgres")
 KNOWLEDGE_GRAPH_DATABASE = os.getenv("KNOWLEDGE_GRAPH_DATABASE", "postgres")
-os.environ["PGPASSWORD"] = os.getenv("KNOWLEDGE_GRAPH_PASSWORD", "postgres")
-
-
-def store_backup_to_ceph(backup_file_dest: str, backup_file_name: str) -> str:
-    """Upload the backup file to ceph."""
-    adapter = GraphBackupStore()
-    adapter.connect()
-    document_id = adapter.store_file(backup_file_dest, backup_file_name)
-    return document_id
 
 
 def main():
     """Perform graph backup job."""
     _LOGGER.debug("Debug mode is on.")
+    adapter = GraphBackupStore()
+    adapter.connect()
 
-    backup_file_name = f"pg_dump-{datetime.now().strftime('%s')}"
-    _LOGGER.info("The database backup would be taken into: %r.", backup_file_name)
-    backup_file_dest = os.path.abspath(backup_file_name)
-
-    _LOGGER.info("Starting the subprocess to take the dump from database.")
+    _LOGGER.info("Starting creation of the database dump")
     run_command(
-        f"pg_dump -h {KNOWLEDGE_GRAPH_HOST} -p {KNOWLEDGE_GRAPH_PORT} -U {KNOWLEDGE_GRAPH_USER} -d {KNOWLEDGE_GRAPH_DATABASE} -f {backup_file_dest}", timeout=None
+        f"pg_dump -h {KNOWLEDGE_GRAPH_HOST} -p {KNOWLEDGE_GRAPH_PORT} "
+        f"-U {KNOWLEDGE_GRAPH_USER} -d {KNOWLEDGE_GRAPH_DATABASE} -f pg_dump.sql",
+        env={"PGPASSWORD": os.getenv("KNOWLEDGE_GRAPH_PASSWORD", "postgres")},
+        timeout=None,
     )
-    _LOGGER.info("upload the database backup file to ceph storage.")
-    try:
-        document_id = store_backup_to_ceph(backup_file_dest, backup_file_name)
-        _LOGGER.info("The database backup to document: %r, is complete in ceph.", document_id)
-    except Exception as e:
-        _LOGGER.exception("An error occurred while uploading backup to ceph: %r", str(e))
-        sys.exit(1)
-
+    _LOGGER.info("Uploading the database dump")
+    object_id = adapter.store_dump("pg_dump.sql")
+    _LOGGER.info("The database dump is available at %s/%s", adapter.prefix, object_id)
     _LOGGER.info("Graph backup task is done.")
 
 
 if __name__ == "__main__":
-    print(f"graph-backup-job v{__version__} starting...")
+    _LOGGER.info("graph-backup-job v%s starting...", __version__)
     main()
